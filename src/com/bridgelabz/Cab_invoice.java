e9a02d4eae0008b97f01e5ee87597caa98f1b80e
maheshnehere <maheshnehere93@gmail.com>
2023-05-02 14:33:16 +0530

    PremiumRides

--- src/com/bridgelabz/Cab_invoice.java
+++ src/com/bridgelabz/Cab_invoice.java
@@ -5,15 +5,24 @@ public class Cab_invoice {
     public static void main(String[] args) {
         HashMap<Double,Double> map = new HashMap();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of rides");
-        double rides = sc.nextDouble();
-        double costPerKm = 10;
-        double costPermin = 1;
-        double minFare = 5;
+
+        double costPerKmForNormalRide = 10;
+        double costPerKmForPremiumRide = 15;
+        double costPerminForNormalRide = 1;
+        double costPerminForPremiumRide = 2;
+        double minFareForNormalRide = 5;
+        double minFareForPremiumRide = 20;
         double totalDistance =0;
         double totalTime =0;
-        double totalFare;
+        double totalFare = 0;
+        double averageFair = 0;
         int count =0;
+
+        System.out.println("Select ride from the following , Press 1.Normal Ride 2.Premium Ride");
+        int option = sc.nextInt();
+
+        System.out.println("Enter number of rides");
+        double rides = sc.nextDouble();
 
         for (int i = 1; i <= rides; i++) {
             System.out.println("RIDE ="+i);
@@ -29,9 +38,13 @@ public class Cab_invoice {
             count++;
         }
 
-        totalFare = (totalDistance * costPerKm) + totalTime + (minFare * count);
-        double averageFair = totalFare/count;
-
+        if (option == 1) {
+            totalFare = (totalDistance * costPerKmForNormalRide) + (totalTime * costPerminForNormalRide) + (minFareForNormalRide * count);
+            averageFair = totalFare / count;
+        }else if (option == 2){
+            totalFare = (totalDistance * costPerKmForPremiumRide) + (totalTime * costPerminForPremiumRide) + (minFareForPremiumRide * count);
+            averageFair = totalFare / count;
+        }
         System.out.println("List of All rides (Distance = Time) respectively =");
         System.out.println(map);
         System.out.println("Total Fare is = " + totalFare);
