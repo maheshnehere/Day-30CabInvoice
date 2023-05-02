6ebb19e922d7bbf407d38270fe46aa1f049e937b
maheshnehere <maheshnehere93@gmail.com>
2023-05-02 14:07:30 +0530

    Welcome to cab invoice program

---
+++ src/com/bridgelabz/Cab_invoice.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class Cab_invoice {
+    public static void main(String[] args) {
+        System.out.println("Welcome to cab invoice generator program");
+    }
+}
