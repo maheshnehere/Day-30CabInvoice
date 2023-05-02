2b4870cebc51b48e5ac518e661f69d15debe117f
maheshnehere <maheshnehere93@gmail.com>
2023-05-02 14:31:10 +0530

    ListofallRides

--- src/com/bridgelabz/Cab_invoice.java
+++ src/com/bridgelabz/Cab_invoice.java
@@ -1,7 +1,9 @@
 package com.bridgelabz;
+import java.util.HashMap;
 import java.util.Scanner;
 public class Cab_invoice {
     public static void main(String[] args) {
+        HashMap<Double,Double> map = new HashMap();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of rides");
         double rides = sc.nextDouble();
@@ -20,16 +22,18 @@ public class Cab_invoice {
             System.out.println("Enter the time in minutes.");
             double time = sc.nextDouble();
 
+            map.put(distance,time);
+
             totalDistance += distance;
             totalTime += time;
             count++;
         }
 
-
         totalFare = (totalDistance * costPerKm) + totalTime + (minFare * count);
-
         double averageFair = totalFare/count;
 
+        System.out.println("List of All rides (Distance = Time) respectively =");
+        System.out.println(map);
         System.out.println("Total Fare is = " + totalFare);
         System.out.println("Total Number of Rides = "+count);
         System.out.println("Average Fare per ride = "+averageFair);
