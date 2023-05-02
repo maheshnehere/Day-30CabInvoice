c7716bc69ec8a17ddc1d3f6110d766cdc357ccb1
maheshnehere <maheshnehere93@gmail.com>
2023-05-02 14:23:20 +0530

    Multiple Rides

--- src/com/bridgelabz/Cab_invoice.java
+++ src/com/bridgelabz/Cab_invoice.java
@@ -3,18 +3,29 @@ import java.util.Scanner;
 public class Cab_invoice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the distance in km");
-        double distance = sc.nextDouble();
-        System.out.println("Enter the time in minutes.");
-
-        //Uc1 - Calculate Fare.
-        double time = sc.nextDouble();
-        double totalFare;
-
+        System.out.println("Enter number of rides");
+        double rides = sc.nextDouble();
         double costPerKm = 10;
         double costPermin = 1;
         double minFare = 5;
-        totalFare = (distance * costPerKm) + time + minFare;
+        double totalDistance =0;
+        double totalTime =0;
+        double totalFare;
+        int count =0;
+
+        for (int i = 1; i <= rides; i++) {
+            System.out.println("RIDE ="+i);
+            System.out.println("Enter the distance in km");
+            double distance = sc.nextDouble();
+            System.out.println("Enter the time in minutes.");
+            double time = sc.nextDouble();
+
+            totalDistance += distance;
+            totalTime += time;
+            count++;
+        }
+
+        totalFare = (totalDistance * costPerKm) + totalTime + (minFare * count);
         System.out.println("Total Fare is = " + totalFare);
     }
     }
