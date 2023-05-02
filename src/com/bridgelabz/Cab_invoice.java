b08ae9fc87d278751efd2b593dfef63698391af0
maheshnehere <maheshnehere93@gmail.com>
2023-05-02 14:11:54 +0530

    Calculate Fare

--- src/com/bridgelabz/Cab_invoice.java
+++ src/com/bridgelabz/Cab_invoice.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class Cab_invoice {
     public static void main(String[] args) {
-        System.out.println("Welcome to cab invoice generator program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the distance in km");
+        double distance = sc.nextDouble();
+        System.out.println("Enter the time in minutes.");
+
+        //Uc1 - Calculate Fare.
+        double time = sc.nextDouble();
+        double totalFare;
+
+        double costPerKm = 10;
+        double costPermin = 1;
+        double minFare = 5;
+        totalFare = (distance * costPerKm) + time + minFare;
+        System.out.println("Total Fare is = " + totalFare);
     }
-}
+    }
